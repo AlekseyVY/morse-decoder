10cfb5179e971a0f2907604483259fa55cbde411
Aleksey Vasiliev <alekseyvy@gmail.com>
2020-12-24 21:05:28 +0300

    feature: task completed

--- src/index.js
+++ src/index.js
@@ -38,9 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let data = [];
+  let str = [];
+  let tmp;
+  let decode = [];
+  for(let i = 0; i < expr.length; i+= 10){
+    data.push(expr.substr(i, 10));
+  };
+  let data_rem = data.map((ele) => ele.replace(/^0+/, ""));
+  for(let elem of data_rem){
+    str = [];
+    tmp = [];
+    for(let i = 0; i < elem.length; i+= 2){
+      if(elem[i] !== "*"){
+        tmp.push(elem.substr(i, 2))
+      }
+    }
+    if(tmp.length < 1){
+      decode.push(" ")
+      continue
+    }
+    for(let i = 0; i < tmp.length; i++){
+      if(tmp[i] == "10"){
+        str.push(".")
+      } 
+      else if(tmp[i] == "11"){
+        str.push("-")
+      }
+    }
+    decode.push(MORSE_TABLE[str.join("")])
+  }
+  return decode.join("")
 }
 
 module.exports = {
     decode
 }
+
+
